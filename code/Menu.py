72ef5a3901b8d62b50fde9fbcddb57350ad4b7f0
jamily <jamilysouzarosa5002@gmail.com>
2026-03-19 21:06:17 -0300

    2° commit

---
+++ code/Menu.py
@@ -0,0 +1,76 @@
+import pygame
+import pygame.image
+
+from pygame import Surface, Rect
+from pygame.font import Font
+
+# CORES
+C_WHITE = (255, 255, 255)
+C_YELLOW = (255, 255, 0)
+
+# TAMANHO DA TELA
+WIN_WIDTH = 800
+
+# OPÇÕES DO MENU
+MENU_OPTION = ("START", "EXIT")
+
+
+class Menu:
+    def __init__(self, window):
+        self.window = window
+        self.surf = pygame.image.load('./asset/fundo menu.png').convert()
+        self.boar = pygame.image.load('./asset/javali.png').convert_alpha()
+        self.boar = pygame.transform.scale(self.boar, (100, 100))
+        self.rect = self.surf.get_rect(left=0, top=0)
+
+    def run(self):
+        menu_option = 0
+
+
+        while True:
+            # DESENHAR FUNDO
+            self.window.blit(self.surf, self.rect)
+            self.window.blit(self.boar, (97, 200))
+
+            # TÍTULO
+            self.menu_text(50, "CITY SCAPE", C_WHITE, (WIN_WIDTH / 2, 100))
+
+            # OPÇÕES
+            for i in range(len(MENU_OPTION)):
+                if i == menu_option:
+                    self.menu_text(30, MENU_OPTION[i], C_YELLOW, (WIN_WIDTH / 2, 200 + 40 * i))
+                else:
+                    self.menu_text(30, MENU_OPTION[i], C_WHITE, (WIN_WIDTH / 2, 200 + 40 * i))
+
+            # CONTROLES
+            self.menu_text(20, "ESPAÇO - PULAR", C_WHITE, (WIN_WIDTH / 2, 350))
+
+            pygame.display.flip()
+
+            # EVENTOS
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_DOWN:
+                        if menu_option < len(MENU_OPTION) - 1:
+                            menu_option += 1
+                        else:
+                            menu_option = 0
+
+                    if event.key == pygame.K_UP:
+                        if menu_option > 0:
+                            menu_option -= 1
+                        else:
+                            menu_option = len(MENU_OPTION) - 1
+
+                    if event.key == pygame.K_RETURN:
+                        return MENU_OPTION[menu_option]
+
+    def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
+        text_font: Font = pygame.font.SysFont("comicsansms", text_size)
+        text_surf: Surface = text_font.render(text, True, text_color)
+        text_rect: Rect = text_surf.get_rect(center=text_center_pos)
+        self.window.blit(text_surf, text_rect)
--- main.py
+++ main.py
@@ -1,14 +1,29 @@
+from code.Menu import Menu
+from code.Const import WIN_WIDTH, WIN_HEIGHT
+
 import pygame
 
 print("Setup start")
 pygame.init()
-window = pygame.display.set_mode(size=(600, 480))
+window = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 print("Setup end")
 
 print("Loop start")
+
+from code.Game import Game
+
+menu = Menu(window)
+option = menu.run()
+
+if option == "START":
+    game = Game(window)
+    game.run()
+
+print(option)
 while True:
     #Check for all events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit() #Close window
             quit() #end pygame
+
